eb2799e577bdfd26b06107e5d1442ad880a8be83
NormalNotNormal <64470987+NormalNotNormal@users.noreply.github.com>
2021-11-22 10:31:56 +0400

    Delete LibraryValidator.java

--- LibraryValidator.java
+++
@@ -1,8 +0,0 @@
-import org.w3c.dom.*;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface LibraryValidator extends Remote {
-    int getCountPriceAverage(Element bookcase) throws RemoteException;
-}
